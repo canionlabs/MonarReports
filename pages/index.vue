9dbd7bad4aff1392ed663959fd0c199a487366cd
caiovictormc <caiovictormc@gmail.com>
2018-12-06 18:54:07 -0300

    Login Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,118 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        monar-reports
-      </h1>
-      <h2 class="subtitle">
-        Reports App
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <a-row class="top-content" type="flex" justify="center" align="middle">
+      <img src="~/assets/logo-monar.png" width="250"/>
+    </a-row>
+    <a-row class="content" type="flex" justify="center">
+      <a-form layout='vertical' :autoFormCreate="(form)=>{this.form = form}">
+        <template v-if="form">
+          <a-form-item
+            :validateStatus="userNameError() ? 'error' : ''"
+            :help="userNameError() || ''"
+            fieldDecoratorId="userName"
+            :fieldDecoratorOptions="{rules: [{ required: true, message: 'Informe seu usuário!' }]}"
+          >
+            <a-input placeholder='Usuário'>
+              <a-icon slot="prefix" type='user' style="color:rgba(0,0,0,.25)"/>
+            </a-input>
+          </a-form-item>
+          <a-form-item
+            :validateStatus="passwordError() ? 'error' : ''"
+            :help="passwordError() || ''"
+            fieldDecoratorId="password"
+            :fieldDecoratorOptions="{rules: [{ required: true, message: 'Informe sua senha!' }]}"
+          >
+            <a-input type='password' placeholder='Senha'>
+              <a-icon slot="prefix" type='lock' style="color:rgba(0,0,0,.25)"/>
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button
+              class="color-orange"
+              type='primary'
+              htmlType='submit'
+              :disabled="hasErrors(form.getFieldsError())"
+            >
+              Entrar
+            </a-button>
+          </a-form-item>
+        </template>
+      </a-form>
+    </a-row>
   </section>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 
+function hasErrors (fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field])
+}
+
 export default {
   components: {
     Logo
-  }
+  },
+  data() {
+    return {
+      hasErrors,
+      form: null
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      // To disabled submit button at the beginning.
+      this.form.validateFields()
+    })
+  },
+  methods: {
+    // Only show error after a field is touched.
+    userNameError () {
+      const { getFieldError, isFieldTouched } = this.form
+      return isFieldTouched('userName') && getFieldError('userName')
+    },
+    // Only show error after a field is touched.
+    passwordError () {
+      const { getFieldError, isFieldTouched } = this.form
+      return isFieldTouched('password') && getFieldError('password')
+    },
+    handleSubmit  (e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values)
+        }
+      })
+    },
+  },
 }
 </script>
 
 <style>
 .container {
   min-height: 100vh;
-  display: flex;
+  padding: 10vh;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.top-content {
+  margin-bottom: 50px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.ant-btn-primary {
+  background-color: #FF6E40;
+  border-color: #FF6E40;
 }
 
-.links {
-  padding-top: 15px;
+.ant-btn-primary:hover {
+  background-color: #e56339;
+  border-color: #e56339;
+}
+
+.ant-btn-primary:focus {
+  background-color: #ff8b66;
+  border-color: #ff8b66;
 }
 </style>
