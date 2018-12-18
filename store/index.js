3c08c0dc03f0c6c6216b39043dd06447c4e53684
caiovictormc <caiovictormc@gmail.com>
2018-12-18 17:46:16 -0300

    setting vuex-persistence

--- store/index.js
+++ store/index.js
@@ -1,16 +1,24 @@
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 const createStore = () => {
   return new Vuex.Store({
     strict: false,
-    state: () => ({
+    state: {
       user: null,
       token: null
-    }),
+    },
+    plugins: [createPersistedState()],
     mutations: {
-      setUser(state, user, token) {
+      setUser(state, [user, token]) {
         state.user = user || null
-        state.user = token || null
+        state.token = token || null
+        console.log(state.user)
+        console.log(state.token)
+      },
+      destroyUser(state) {
+        state.user = null
+        state.token = null
       }
     },
     getters: {
@@ -22,8 +30,11 @@ const createStore = () => {
       }
     },
     actions: {
-      login ({commit}, username, token) {
-        commit('setUser', username, token)
+      login ({commit}, {username, token}) {
+        commit('setUser', [username, token])
+      },
+      logout ({commit}) {
+        commit('destroyUser')
       }
     }
   })
